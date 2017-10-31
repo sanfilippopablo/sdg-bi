2a5486e9f3b4d40add3e81940a743f25363de3cf
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2017-10-31 16:21:04 -0300

    Last version

--- tdc_staging.sql
+++ tdc_staging.sql
@@ -1,171 +1,159 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `tdc_staging` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `tdc_staging`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: localhost
--- Generation Time: Oct 31, 2017 at 06:05 PM
--- Server version: 5.7.19-0ubuntu0.16.04.1
--- PHP Version: 7.0.22-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: tdc_staging
+-- ------------------------------------------------------
+-- Server version	5.7.14
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `tdc_staging`
---
-CREATE DATABASE IF NOT EXISTS `tdc_staging` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `tdc_staging`;
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `detalle_ventas`
 --
 
 DROP TABLE IF EXISTS `detalle_ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `detalle_ventas` (
   `cod_venta` int(10) NOT NULL,
   `cod_producto` smallint(5) NOT NULL,
-  `cantidad` smallint(5) DEFAULT NULL
+  `cantidad` smallint(5) DEFAULT NULL,
+  PRIMARY KEY (`cod_venta`,`cod_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `empleados`
 --
 
 DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `empleados` (
   `cod_vendedor` int(11) NOT NULL,
   `nombre` varchar(445) DEFAULT NULL,
   `fecha_ingreso` date DEFAULT NULL,
-  `genero` varchar(45) DEFAULT NULL
+  `genero` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_vendedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `mayoristas`
 --
 
 DROP TABLE IF EXISTS `mayoristas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mayoristas` (
   `nombre` varchar(150) DEFAULT NULL,
   `cod_cliente` int(11) NOT NULL,
   `fecha_nac` date NOT NULL,
-  `cod_region` int(11) NOT NULL
+  `cod_region` int(11) NOT NULL,
+  PRIMARY KEY (`cod_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `minoristas`
 --
 
 DROP TABLE IF EXISTS `minoristas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `minoristas` (
   `cod_cliente` int(11) NOT NULL,
   `nombre` varchar(150) DEFAULT NULL,
   `fecha_nac` date NOT NULL,
-  `cod_region` int(11) NOT NULL
+  `cod_region` int(11) NOT NULL,
+  PRIMARY KEY (`cod_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `productos`
 --
 
 DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `productos` (
   `cod_producto` int(11) NOT NULL,
   `descripcion` varchar(150) DEFAULT NULL,
-  `presentacion` varchar(100) DEFAULT NULL
+  `presentacion` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cod_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `regiones`
 --
 
 DROP TABLE IF EXISTS `regiones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `regiones` (
   `cod_region` int(11) NOT NULL,
   `region` varchar(45) DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
-  `ciudad` varchar(45) DEFAULT NULL
+  `ciudad` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_region`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `stagingventas`
+--
+
+DROP TABLE IF EXISTS `stagingventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stagingventas` (
+  `fecha` date NOT NULL,
+  `cod_cliente` int(11) NOT NULL,
+  `cod_vendedor` int(11) NOT NULL,
+  `LitrosEnvase` float NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `region` varchar(45) NOT NULL,
+  PRIMARY KEY (`cod_cliente`,`fecha`,`LitrosEnvase`,`cod_vendedor`,`region`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `ventas`
 --
 
 DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ventas` (
   `cod_venta` int(10) NOT NULL,
   `region` varchar(45) DEFAULT NULL,
   `cod_sucursal` int(10) DEFAULT NULL,
   `fecha` datetime DEFAULT NULL,
   `cod_cliente` smallint(5) DEFAULT NULL,
-  `cod_empleado` smallint(5) DEFAULT NULL
+  `cod_empleado` smallint(5) DEFAULT NULL,
+  PRIMARY KEY (`cod_venta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `detalle_ventas`
---
-ALTER TABLE `detalle_ventas`
-  ADD PRIMARY KEY (`cod_venta`,`cod_producto`);
-
---
--- Indexes for table `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`cod_vendedor`);
-
---
--- Indexes for table `mayoristas`
---
-ALTER TABLE `mayoristas`
-  ADD PRIMARY KEY (`cod_cliente`);
-
---
--- Indexes for table `minoristas`
---
-ALTER TABLE `minoristas`
-  ADD PRIMARY KEY (`cod_cliente`);
-
---
--- Indexes for table `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`cod_producto`);
-
---
--- Indexes for table `regiones`
---
-ALTER TABLE `regiones`
-  ADD PRIMARY KEY (`cod_region`);
-
---
--- Indexes for table `ventas`
---
-ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`cod_venta`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-31 16:20:10
