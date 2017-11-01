bc8d211d7350b1e78dae1eb26c8b6231676d964d
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2017-11-01 15:22:06 -0300

    Added db structure

--- dbs.sql
+++ dbs.sql
@@ -1,3 +1,115 @@
+CREATE DATABASE  IF NOT EXISTS `tdc_dw` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `tdc_dw`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tdc_dw
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `cod_cliente` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `cod_region` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cod_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productos` (
+  `cod_producto` int(11) NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `presentacion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `regiones`
+--
+
+DROP TABLE IF EXISTS `regiones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `regiones` (
+  `cod_region` int(11) NOT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  `ciudad` varchar(45) DEFAULT NULL,
+  `region` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_region`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vendedores`
+--
+
+DROP TABLE IF EXISTS `vendedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vendedores` (
+  `cod_vendedor` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `fecha_ingreso` date DEFAULT NULL,
+  `genero` varchar(45) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  PRIMARY KEY (`cod_vendedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ventas`
+--
+
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ventas` (
+  `cod_cliente` int(11) NOT NULL,
+  `cod_vendedor` int(11) NOT NULL,
+  `litros` float DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `cod_tiempo` date NOT NULL,
+  `cod_producto` int(11) NOT NULL,
+  `cod_region` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cod_cliente`,`cod_tiempo`,`cod_vendedor`,`cod_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-01 15:21:36
 CREATE DATABASE  IF NOT EXISTS `tdc_staging` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `tdc_staging`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
@@ -16,6 +128,23 @@ USE `tdc_staging`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `cod_cliente` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `fecha_nac` date DEFAULT NULL,
+  `cod_region` int(11) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `detalle_ventas`
@@ -44,6 +173,7 @@ CREATE TABLE `empleados` (
   `nombre` varchar(445) DEFAULT NULL,
   `fecha_ingreso` date DEFAULT NULL,
   `genero` varchar(45) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
   PRIMARY KEY (`cod_vendedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -122,10 +252,10 @@ CREATE TABLE `stagingventas` (
   `fecha` date NOT NULL,
   `cod_cliente` int(11) NOT NULL,
   `cod_vendedor` int(11) NOT NULL,
-  `LitrosEnvase` float NOT NULL,
+  `LitrosEnvase` float DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
-  `region` varchar(45) NOT NULL,
-  PRIMARY KEY (`cod_cliente`,`fecha`,`LitrosEnvase`,`cod_vendedor`,`region`)
+  `cod_producto` int(11) NOT NULL,
+  PRIMARY KEY (`cod_cliente`,`fecha`,`cod_vendedor`,`cod_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -156,4 +286,4 @@ CREATE TABLE `ventas` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-31 16:20:10
+-- Dump completed on 2017-11-01 15:21:37
